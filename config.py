60fb350677198628b17dd6b3b87becaa0dd48e0e
brian.khuong <brian.khuong@gmail.com>
2025-11-16 21:59:05 +0700

    Fix redirect URI configuration

--- config.py
+++ config.py
@@ -12,5 +12,6 @@ class Config:
     CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
     AUTHORITY = "https://login.microsoftonline.com/common"
     SCOPE = ["https://graph.microsoft.com/Mail.Read", "https://graph.microsoft.com/Mail.ReadWrite", "https://graph.microsoft.com/User.Read"]
-    # Railway will provide the REDIRECT_URI via environment variable
+    
+    # Use the Railway provided URL - don't hardcode it
     REDIRECT_URI = os.environ.get('REDIRECT_URI', 'http://localhost:5001')
